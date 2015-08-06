50f9e4661f31a067d1ff612138c0b6046ebd464c
Taras Mankovski <tarasm@gmail.com>
2015-08-06 11:28:38 -0400

    Initial commit

--- app/router.js
+++ app/router.js
@@ -6,6 +6,10 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('posts', function(){
+    this.route('new');
+    this.route('edit', {path: ':id/edit'});
+  });
 });
 
 export default Router;
